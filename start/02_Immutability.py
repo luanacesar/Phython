05a6c6b80f4dc8bd9f8eedaa87ac6b70f7f0d0d3
luanacesar <luana_cesar@hotmail.com>
2021-01-11 13:04:03 -0500

    concatenate

---
+++ start/02_Immutability.py
@@ -0,0 +1,14 @@
+name = "sam"
+n ='P'
+last_letters = name[1:]
+
+complete_name = n + last_letters
+
+# print name normal
+print(complete_name)
+
+# print name all upper case
+print(complete_name.upper())
+
+# capitalize just the first letter
+print(complete_name.capitalize())
