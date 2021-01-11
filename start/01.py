213a6413c38d26e189669ea35b973b195b63ea0b
luanacesar <luana_cesar@hotmail.com>
2021-01-11 12:43:38 -0500

    reverse string

--- start/01.py
+++ start/01.py
@@ -1,6 +1,18 @@
 mystring = "hello world"
 int = 5
 print(mystring)
+
 print(mystring[0])
+
+
+# starting with 3 until de end 3 
 print(mystring[2:])
-print(mystring[0:5])
+
+# jumping 3 to 3
+print(mystring[::3]) 
+
+# all to way from 0 to end but step side -1  (reverse string)
+print(mystring[::-1])
+
+# 0 to end 
+print(mystring[:])
