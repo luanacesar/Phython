71391eb8a6a37803069bb93aea266f9280925642
luanacesar <luana_cesar@hotmail.com>
2021-01-12 18:38:29 -0500

    dictionaries

---
+++ Python_Bootcamp-Exercises/dictionaries.py
@@ -0,0 +1,9 @@
+# Dictionaries can be retrieved by key name
+my_dict = {'key1':'value1', 'key2': 'value2'}
+print(my_dict)
+print(my_dict['key1'])
+
+products = {'apple':2.99, 'oranges': 1.99, 'milk':5.8}
+print(products['oranges'])
+
+d ={'k1':123,'k2':[0,1,2],'k3':{}}
