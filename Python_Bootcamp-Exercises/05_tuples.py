091764414d85f3185b3a39c249738a07af7a60d3
luanacesar <luana_cesar@hotmail.com>
2021-01-14 19:05:26 -0500

    TUPLES

---
+++ Python_Bootcamp-Exercises/05_tuples.py
@@ -0,0 +1,19 @@
+# Tuples are used to store multiple items in a single variable.
+# Tuple is one of 4 built-in data types in Python used to store collections of data,
+# the other 3 are List, Set, and Dictionary, 
+# all with different qualities and usage. A tuple is a collection which is ordered and unchangeable.
+# you declare tuples in ()
+
+
+t = (1,2,3)
+myList = [1,2,3]
+print(type(t))
+print(len(t))
+
+t =(0,0,0,2,3)
+
+print(t.count('a'))
+
+print(t.index('a'))
+
+t.
